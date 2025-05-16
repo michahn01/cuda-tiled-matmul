ce34e664b46dd87136f748242a18fd95b7b243bf
Jisang Ahn <michahn@umich.edu>
2025-05-15 21:49:48 -0700

    Add a small matmul test

---
+++ tests/test_matmul.cpp
@@ -0,0 +1,30 @@
+#include <cassert>
+#include <vector>
+
+#include "matrix.h"
+
+void test_matmul_simple() {
+    std::vector<std::vector<float>> A = {
+        {-1, -1, -1},
+        {2, 2, 2},
+        {0, 0, 0},
+        {1, 0, -1}
+    };
+    std::vector<std::vector<float>> B = {
+        {1, 1},
+        {0, 2},
+        {0, -1},
+    };
+
+    Matrix C_cpu = A.matmul(B, Matrix::MatMulType::CPU);
+    Matrix C_untiled_gpu = A.matmul(B, Matrix::MatMulType::GPU_NO_TILING);
+    Matrix C_tiled_gpu = A.matmul(B, Matrix::MatMulType::GPU_TILING);
+    
+    assert(C_cpu == C_untiled_gpu);
+    assert(C_untiled_gpu == C_tiled_gpu);
+    assert(C_cpu == C_tiled_gpu);
+}
+
+int main() {
+    test_matmul_simple();
+}
