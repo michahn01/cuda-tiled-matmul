a9eb0cdb25bfe0a449dddf31749873c7436e2c8e
Jisang Ahn <michahn@umich.edu>
2025-05-15 20:38:39 -0700

    Update comments in header

--- include/matrix.h
+++ include/matrix.h
@@ -5,7 +5,10 @@
 class Matrix {
 public:
 
-    /* --- CONSTRUCTORS --- */
+    /* -------------------------------------------------------------------- */
+    /* -------------------------- CONSTRUCTORS ---------------------------- */
+    /* -------------------------------------------------------------------- */
+
     /* Construct a 0x0 matrix with no data. */
     Matrix();
 
@@ -20,40 +23,47 @@ public:
 
     /* Construct an (N x 1) column vector from the given 1D vector. */
     Matrix(const std::vector<float>& col_vec);
-    /* --- END --- */
 
 
-    /* --- MEMORY MANAGEMENT (rule of five) --- */
+    /* -------------------------------------------------------------------- */
+    /* ---------------- MEMORY MANAGEMENT (rule of five) ------------------ */
+    /* -------------------------------------------------------------------- */
+
     ~Matrix();
     Matrix(const Matrix& other);
     Matrix& operator=(const Matrix& other);
     Matrix(Matrix&& other) noexcept;
     Matrix& operator=(Matrix&& other) noexcept;
-    /* --- END --- */
 
 
-    /* --- UTILITY --- */
+    /* -------------------------------------------------------------------- */
+    /* ---------------------------- UTILITY ------------------------------- */
+    /* -------------------------------------------------------------------- */
+
     /* Note: [] accesses will not be bounds-checked. */
     float* operator[](size_t index);
     const float* operator[](size_t index) const;
+
     bool operator==(const Matrix& other);
     size_t getRows() const;
     size_t getCols() const;
-    /* --- END --- */
 
 
-    /* --- MATRIX MULTIPLICATION */
-    /* RECOMMENDED operator for matrix multiplication; uses GPU and tiling. */
+    /* -------------------------------------------------------------------- */
+    /* ---------------------- MATRIX MULTIPLICATION ----------------------- */
+    /* -------------------------------------------------------------------- */
+
+    /* Primary method for matrix multiplication; uses GPU and performs tiling. */
     Matrix operator*(const Matrix& other);
 
-    /* Matrix multiplier for testing purposes. */
     enum class MatMulType {
         CPU, // utilize CPU for matmul
         GPU_NO_TILING, // utilize GPU for matmul, without tiling
         GPU_TILING // utilize GPU for matmul, with tiling; equivalent to invoking `operator*`
     };
+    /* A matrix multiply method where caller can specify the type of implementation. */
     Matrix matmul(const Matrix& other, MatMulType type);
-    /* --- END --- */
+
 
 private:
     // Internally, data is represented as a flattened row-major array of floats.
